5aef7f76a1a57003efea105d9456402f90013e4e
suparsana <madesuparsana29@gmail.com>
2021-01-01 17:57:44 +0800

    update notification

--- android/app/src/main/kotlin/com/example/acenet_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/acenet_project/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.acenet_project
 
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity() {}
---
+++ lib/views/history_page.dart
@@ -0,0 +1,70 @@
+import 'package:acenet_project/API/ApiServices.dart';
+import 'package:acenet_project/model/SPKModel.dart';
+import 'package:acenet_project/models/index.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'package:acenet_project/drawer/drawer_layout.dart';
+import 'home_page.dart';
+
+
+class TodayTaskPage extends StatefulWidget {
+  @override
+  _TodayTaskPageState createState() => _TodayTaskPageState();
+}
+
+class _TodayTaskPageState extends State<TodayTaskPage> {
+  List list = new List<SpkDetail>();
+  var loading = true;
+
+  _getSPK(int idTeknisi) async {
+    // list.clear();
+
+    await ApiServices().getSPK(idTeknisi).then((value) {
+      setState(() {
+        list = value;
+        loading = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("test");
+    _getSPK(1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Today Task")),
+      drawer: myDrawer(),
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: loading
+            ? Center(child: CircularProgressIndicator())
+            : list.length > 0 ? RefreshIndicator(
+          onRefresh: () async {
+            await _getSPK(1);
+          },
+          child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (context, i) {
+              var data = list[i];
+              return CardLayout(
+                pekerjaan: data.ket_pekerjaan,
+                waktu: data.jam_mulai,
+                pelanggan: data.nama,
+                idPekerjaan: (data.id),
+              );
+            },
+          ),
+        ) : Center(
+          child: Text("No Task for Today."),
+        ),
+      ),
+    );
+  }
+}
